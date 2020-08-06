92ee496322efedbda97a95f809978551c7d9310c
Nikita Bloshchanevich <nikblos@outlook.com>
2020-08-06 13:48:38 +0200

    Add license

--- dap-mode-launch-json.el
+++ dap-mode-launch-json.el
@@ -1,9 +1,29 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t -*-
+
+;; Copyright (C) 2020 Nikita Bloshchanevich
+
+;; Author: Nikita Bloshchanevich <nikblos@outlook.com>
+;; Keywords: languages
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 (require 'dap-mode)
 (require 'cl-lib)
 (require 'projectile)
 
 ;;; Code:
+
 (defun dap--projectile-find-launch-json ()
   "Return the location of the launch.json file in the current project."
   (when-let ((project (projectile-ensure-project (projectile-project-root))))
